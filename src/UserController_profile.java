f5981105fceb630b815d829e2497d7cb6317c9d7
Anas-mo101 <77382332+Anas-mo101@users.noreply.github.com>
2021-11-15 18:09:29 +0800

    Add files via upload

---
+++ src/UserController_profile.java
@@ -0,0 +1,52 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+
+public class UserController_profile extends Pane {
+    Csvreader csv = new Csvreader("student.csv");
+    private String[] userData; 
+
+    @FXML
+    private Label studIDlbl;
+
+    @FXML
+    private Label emaillbl;
+
+    @FXML
+    private Label licencetypelbl;
+
+    @FXML
+    private Label agelbl;
+
+    @FXML
+    private Label idTypelbl;
+
+    @FXML
+    private Label namelbl;
+
+    @FXML
+    private Label idNolbl;
+
+    @FXML
+    private Label phonelbl;
+
+    @FXML
+    private Label nationallbl;
+
+    public void setUserdata(String[] userdata){
+        userData = csv.GetUserInfoByUsername(userdata[0]);
+        namelbl.setText(userData[0]);
+        studIDlbl.setText(userData[3]);
+        agelbl.setText(userData[4]);
+        phonelbl.setText(userData[5]);
+        emaillbl.setText(userData[6]);
+        idTypelbl.setText(userData[7]);
+        idNolbl.setText(userData[8]);
+        nationallbl.setText(userData[9]);
+        licencetypelbl.setText(userData[10]); 
+    }
+
+}
+
+
